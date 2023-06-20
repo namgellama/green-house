9565958593d4ebdc40a8666c9a356acf1905eed8
namgellama <namgeltamang14@gmail.com>
2023-06-20 23:13:48 +0545

    Initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,76 @@
+const Home = () => {
+    return (
+        <>
+            <div className="pimg1">
+                <div className="ptext">
+                    <span className="text-2xl md:text-4xl font-bold bg-[#000] lg:p-4 text-white">
+                        Welcome To Heavenly Green House
+                    </span>
+                </div>
+            </div>
+            <section className="section section-light p-8 lg:p-20">
+                <h2 className="text-3xl ">Welcome</h2>
+                <p className="text-justify mt-6 text-xl">
+                    Welcome to Heavenly Green House Pvt. Ltd., an enchanting
+                    destination where nature's wonders unfold in all their
+                    splendor. As a premier greenhouse establishment, we take
+                    pride in our commitment to sustainability, innovation, and
+                    the extraordinary beauty that plants bring into our lives.
+                    Step into our verdant oasis and embark on a journey of
+                    exploration and inspiration.
+                </p>
+            </section>
+            <div className="pimg2"></div>
+            <section className="section section-dark p-8 lg:p-20">
+                <p className="text-justify mt-4 text-xl">
+                    Heavenly Green House is more than just a greenhouse; it is a
+                    sanctuary where the harmonious coexistence of humans and
+                    nature thrives. Our passionate team of horticulturists,
+                    botanists, and gardening enthusiasts have carefully curated
+                    a diverse selection of plants from every corner of the
+                    world. From vibrant tropical blooms to resilient succulents,
+                    we offer an extensive range of botanical treasures that
+                    cater to every gardening passion and taste.
+                </p>
+            </section>
+            <div className="pimg3"></div>
+            <section className="section section-dark p-8 lg:p-20">
+                <p className="text-justify mt-4 text-xl">
+                    But Heavenly Green House is not just a place to acquire
+                    plants; it's a holistic experience designed to nurture your
+                    love for nature. Wander through our meticulously designed
+                    green spaces, breathe in the intoxicating aromas, and let
+                    the tranquility of our surroundings envelop your senses.
+                    Discover the joys of gardening through our expert advice,
+                    workshops, and educational programs tailored to beginners
+                    and seasoned enthusiasts alike.
+                </p>
+            </section>
+
+            <div className="pimg4">
+                <div className="ptext">
+                    <span className="text-2xl md:text-4xl font-bold bg-[#000] lg:p-4 text-white">
+                        Eat Clean, Eat Green, Eat Organic
+                    </span>
+                </div>
+            </div>
+
+            <section className="section section-dark p-8 lg:p-20">
+                <p className="text-justify mt-4 text-xl">
+                    Join us on a journey where the boundaries between indoor and
+                    outdoor blur, where lush greenery and colorful blooms
+                    transport you to a world of serenity and joy. Indulge in the
+                    wonders of nature and let Heavenly Green House be your
+                    sanctuary, your source of inspiration, and your partner in
+                    creating a greener, more vibrant world. Welcome to a place
+                    where dreams take root and where the extraordinary beauty of
+                    nature unfolds – welcome to Heavenly Green House Pvt. Ltd.
+                </p>
+            </section>
+
+            <div className="pimg5"></div>
+        </>
+    );
+};
+
+export default Home;
